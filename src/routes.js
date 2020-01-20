7663d43561af51a3af3f883a00137a51badd056c
Iury Lemos <iurylemos10@gmail.com>
2020-01-20 01:21:04 -0300

    Criando projeto e rotas com as duas view

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './view/main';
+import Repositorio from './view/respository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/repositorio/:repositorio" component={Repositorio} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
